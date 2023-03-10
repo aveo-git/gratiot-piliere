63faf1323c274199b6307633eeee9132ff084985
Dimby Rasolonirina <dimbyrasolonirina@gmail.com>
2023-03-10 17:20:58 +0300

    Add: noproduct comp

--- client/src/containers/ProductDisplay.js
+++ client/src/containers/ProductDisplay.js
@@ -1,6 +1,8 @@
+import classNames from 'classnames'
 import React from 'react'
 import { createUseStyles } from 'react-jss'
 import { useGetProducts } from '../api/product.api'
+import NoProducts from './NoProducts'
 import ProductItem from './product/ProductItem'
 
 const useStyles = createUseStyles(theme => ({
@@ -9,6 +11,11 @@ const useStyles = createUseStyles(theme => ({
         height: 'calc(100vh - 190px)',
         overflow: 'auto',
         paddingRight: 10
+    },
+    noProduct: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     productsItem: {
         display: 'grid',
@@ -23,11 +30,16 @@ const useStyles = createUseStyles(theme => ({
 const ProductDisplay = () => {
     const classes = useStyles()
     const { products } = useGetProducts()
+    const isProductEmpty = products.length === 0
     return (
-        <div className={classes.root}>
-            <div className={classes.productsItem}>
-                {products.map(product => <ProductItem product={product} />)}
-            </div>
+        <div className={classNames(classes.root, {[classes.noProduct]: isProductEmpty})}>
+            { isProductEmpty ? 
+                <NoProducts info='Pas de produits selectionné'/>
+            : 
+                <div className={classes.productsItem}>
+                    {products.map(product => <ProductItem product={product} />)}
+                </div>
+            }
         </div>
     )
 }
